3635459435c1ebea584b4bf8e57eea51a8e6fee8
Sadhana@Windows <sadhana.v360@gmail.com>
2021-11-25 12:06:00 +0530

    updating

--- app/Validators/CreatePostValidator.ts
+++ app/Validators/CreatePostValidator.ts
@@ -20,6 +20,7 @@ export default class CreatePostValidator {
     'required': 'The {{ field }} is required',
     'title.maxLength': 'Title can not be longer than 50 characters',
     'description.maxLength': 'Description can not be longer than 400 characters',
+    'postImage.required': 'The Image is required',
     'postImage.size': 'Image size should not be greater than 2mb',
     'postImage.extnames': 'Image should be jpg or png only',
     'tags.minLength': 'At least one tag should be entered',
